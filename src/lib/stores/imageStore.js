9b5ea736d231e434c79c17577888d87f5751a03b
anito <axelnitzschner@mac.com>
2022-07-25 14:12:41 +0200

    fbLogin

--- src/lib/stores/imageStore.js
+++ src/lib/stores/imageStore.js
@@ -2,35 +2,35 @@
 import { writable } from 'svelte/store';
 
 function createStore() {
-	const { subscribe, update, set } = writable([], () => {});
+  const { subscribe, update, set } = writable([], () => {});
 
-	let findIndex = (id, items) => {
-		return items.findIndex((itm) => itm.id == id);
-	};
-	let sortFunc = (a, b) => {
-		return a.src.toLowerCase() < b.src.toLowerCase()
-			? -1
-			: a.src.toLowerCase() > b.src.toLowerCase()
-			? 1
-			: 0;
-	};
+  let findIndex = (id, items) => {
+    return items.findIndex((itm) => itm.id == id);
+  };
+  let sortFunc = (a, b) => {
+    return a.src.toLowerCase() < b.src.toLowerCase()
+      ? -1
+      : a.src.toLowerCase() > b.src.toLowerCase()
+      ? 1
+      : 0;
+  };
 
-	return {
-		subscribe,
-		add: (val) => update((items) => [...items, ...val].sort(sortFunc)),
-		put: (id) =>
-			update((items) => {
-				const index = findIndex(id, items);
-				return [
-					...items.slice(0, index),
-					{ ...items[index], ...item },
-					...items.slice(index + 1)
-				].sort(sortFunc);
-			}),
-		del: (id) => update((items) => items.filter((itm) => itm.id !== id)),
-		update: (val) => update(() => val.sort(sortFunc)),
-		set
-	};
+  return {
+    subscribe,
+    add: (val) => update((items) => [...items, ...val].sort(sortFunc)),
+    put: (id) =>
+      update((items) => {
+        const index = findIndex(id, items);
+        return [
+          ...items.slice(0, index),
+          { ...items[index], ...item },
+          ...items.slice(index + 1)
+        ].sort(sortFunc);
+      }),
+    del: (id) => update((items) => items.filter((itm) => itm.id !== id)),
+    update: (val) => update(() => val.sort(sortFunc)),
+    set
+  };
 }
 
 export default createStore();
