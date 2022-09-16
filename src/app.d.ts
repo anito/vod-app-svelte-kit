fa761404e808eaa3951fec011539302294b6b118
anito <axelnitzschner@mac.com>
2022-09-16 22:09:15 +0200

    go jsDoc #2

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare namespace App {
-	// interface Locals {}
+  interface Locals {
+    session: import('svelte-kit-cookie-session').Session<SessionData>;
+    usersRepo: import('$lib/repos').UsersRepo,
+    videosRepo: import('$lib/repos').VideosRepo,
+    imagesRepo: import('$lib/repos').ImagesRepo,
+    videosAllRepo: import('$lib/repos').VideosAllRepo,
+  }
 	// interface Platform {}
 	// interface Session {}
 	// interface Stuff {}
