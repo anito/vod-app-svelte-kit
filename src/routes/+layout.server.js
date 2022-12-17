df2654087ad1685c3223efa233408475136ab771
anito <axelnitzschner@mac.com>
2022-12-17 11:06:48 +0100

    added locale prop to many of api calls

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,7 +2,6 @@
 export async function load({ depends, locals }) {
   depends('app:session');
 
-  console.log('load', locals);
   const ua = locals.userAgent;
   return { session: locals.session.data, ua };
 }
