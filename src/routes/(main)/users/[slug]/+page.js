914ac1b0d99a227e685ea73545f83a238d475696
anito <axelnitzschner@mac.com>
2022-12-23 14:35:28 +0100

    fix: stop loading a user from url w/o having sufficent user right

--- src/routes/(main)/users/[slug]/+page.js
+++ src/routes/(main)/users/[slug]/+page.js
@@ -1,23 +1,32 @@
 import { get } from 'svelte/store';
 import { users } from '$lib/stores';
+import { ADMIN, SUPERUSER } from '$lib/utils';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ params, fetch }) {
-  const id = params.slug;
-  /**
-   * @type {never[]}
-   */
-  const $users = get(users);
-  const dataExists = !!$users.find(
-    /** @param {import('$lib/types').User} user */ (user) => user.id === id
-  );
-  if (!dataExists) {
-    return await fetch(`/users/${id}`).then(async (res) => {
-      const { success, data } = await res.json();
-      if (success) {
-        return { user: data };
-      }
-    });
+export async function load({ params, fetch, parent }) {
+  const parentData = await parent();
+  const { role } = parentData.session;
+  const hasPrivileges = role === SUPERUSER || role === ADMIN;
+
+  // load the user in question (from url)
+  // if user rights permit
+  if (hasPrivileges) {
+    /**
+     * @type {never[]}
+     */
+    const $users = get(users);
+    const id = params.slug;
+    const dataExists = !!$users.find(
+      /** @param {import('$lib/types').User} user */ (user) => user.id === id
+    );
+    if (!dataExists) {
+      return await fetch(`/users/${id}`).then(async (res) => {
+        const { success, data } = await res.json();
+        if (success) {
+          return { user: data };
+        }
+      });
+    }
   }
   return { user: null };
 }
