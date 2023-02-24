47ae7c80cb755fa3d42954adf93c2d231c5792a8
anito <axelnitzschner@mac.com>
2023-02-24 05:45:43 +0100

    removed usage of url in (main) layout load

--- src/routes/login/+page.ts
+++ src/routes/login/+page.ts
@@ -3,15 +3,8 @@ import type { PageLoadEvent } from './$types';
 
 export async function load({ parent, fetch, url }: PageLoadEvent) {
   /**
-   * Provided the session had been brought to life from any other browser tab,
-   * this automatically redirects a user away from login  page (to redirect slug or home)
-   * after a revisit (visibiltychange event)
+   * Handles a token login
    */
-  const parentData = await parent();
-  if (parentData.session.user) {
-    throw redirect(301, url.searchParams.get('redirect') || '/');
-  }
-
   const token = url.searchParams.get('token');
   if (token) {
     return await fetch(`/auth/login?token=${token}`).then(async (res) => {
@@ -22,5 +15,16 @@ export async function load({ parent, fetch, url }: PageLoadEvent) {
       };
     });
   }
+
+  /**
+   * Provided the session had been brought to life from any other browser tab,
+   * this automatically redirects a user away from login  page (to redirect slug or home)
+   * after a revisit (visibiltychange event)
+   */
+  const parentData = await parent();
+  if (parentData.session.user) {
+    throw redirect(301, url.searchParams.get('redirect') || '/');
+  }
+
   return {};
 }
