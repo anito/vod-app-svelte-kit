1ecaba722950a07698eedff4d863c788c7e6f698
anito <axelnitzschner@mac.com>
2023-03-04 15:40:25 +0100

    fixed Badge prop for UserGraphic in UserManager

--- src/lib/components/_UserManager.svelte
+++ src/lib/components/_UserManager.svelte
@@ -434,12 +434,12 @@
                     borderColor="--primary"
                     extendedBorderColor="--background-intense"
                     extendedBorderSize={10}
-                    badge={{
-                      icon: hasCurrentPrivileges ? 'admin_panel_settings' : '',
+                    badge={ hasCurrentPrivileges ? {
+                      icon:  'admin_panel_settings',
                       color: isCurrentSuperuser ? 'rgb(26, 4, 4)' : 'rgb(206, 4, 4)',
                       size: 'large',
                       position: 'BOTTOM_RIGHT'
-                    }}
+                    } : false }
                   />
                   <Menu
                     bind:this={avatarMenu}
