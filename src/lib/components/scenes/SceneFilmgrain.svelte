c9953a179a8ccc71c7d7f59a6fb55ec94d7115c6
Axel Nitzschner <axelnitzschner@mac.com>
2023-07-27 09:13:47 +0200

    shader changes

--- src/lib/components/scenes/SceneFilmgrain.svelte
+++ src/lib/components/scenes/SceneFilmgrain.svelte
@@ -1,12 +1,13 @@
 <script>
   import { T } from '@threlte/core';
-  import { OrbitControls } from '@threlte/extras';
   import Filmgrain from '$lib/components/_Filmgrain.svelte';
-
 </script>
 
-<T.PerspectiveCamera aspect={window.innerWidth/window.innerHeight} near={0.1} far={1000} fov={75}>
-  <OrbitControls enabled={false} />
-</T.PerspectiveCamera>
+<T.PerspectiveCamera
+  aspect={window.innerWidth / window.innerHeight}
+  near={0.1}
+  far={1000}
+  fov={75}
+/>
 
 <Filmgrain />
--- src/lib/components/scenes/SceneTerrain.svelte
+++ src/lib/components/scenes/SceneTerrain.svelte
@@ -1,8 +1,6 @@
 <script>
   import { T } from '@threlte/core';
-  import { OrbitControls } from '@threlte/extras';
   import Terrain from '$lib/components/_Terrain.svelte';
-
 </script>
 
 <T.OrthographicCamera
@@ -10,8 +8,6 @@
   far={1000}
   position={[-Math.sqrt(2), Math.sqrt(2), -Math.sqrt(200)]}
   makeDefault
->
-  <OrbitControls enabled={false} />
-</T.OrthographicCamera>
+/>
 
 <Terrain />
